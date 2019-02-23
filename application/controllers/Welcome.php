b9a44874f4a2529b100ef8dd974ee88b2496a699
penglin <plin@qq.com>
2019-02-23 14:17:26 +0800

    1

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,4 +22,24 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+
+	public function webhook()
+	{
+		// GitHub Webhook Secret.
+		// Keep it the same with the 'Secret' field on your Webhooks / Manage webhook page of your respostory.
+		$secret = "penglin";
+		// Path to your respostory on your server.
+		// e.g. "/var/www/respostory"
+		$path = "/www/wwwroot/ci";
+		// Headers deliveried from GitHub
+		$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+		if ($signature) {
+		$hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
+		if (strcmp($signature, $hash) == 0) {
+			echo shell_exec("cd {$path} && git reset --hard origin/master && git clean -f && git pull 2>&1");
+			exit();
+		}
+		}
+		http_response_code(404);
+	}
 }
