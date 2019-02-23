9ee04987f1340eff25a9b87e68634219a240b7f1
penglin <plin@qq.com>
2019-02-23 14:49:52 +0800

    1

--- webhook/webhook_linux.js
+++ webhook/webhook_linux.js
@@ -17,6 +17,8 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
+
+    
 })
  
 handler.on('issues', function (event) {
