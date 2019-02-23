925375e4d743977ec4cc912def50a6901fab0a99
penglin <plin@qq.com>
2019-02-23 14:28:13 +0800

    1

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -31,15 +31,8 @@ class Welcome extends CI_Controller {
 		// Path to your respostory on your server.
 		// e.g. "/var/www/respostory"
 		$path = "/www/wwwroot/ci";
-		// Headers deliveried from GitHub
-		$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-		if ($signature) {
-			$hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
-			if (strcmp($signature, $hash) == 0) {
-				echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-				exit();
-			  }
-		}
-		http_response_code(404);
+
+		echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+		exit();
 	}
 }
