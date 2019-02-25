91ca93df7a53d4f5fb7e76aa42ab87784423c45c
penglin <plin@qq.com>
2019-02-25 16:22:50 +0800

    1

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -11,19 +11,13 @@ class User extends CI_Controller
     public function list(){
         $query = $this->db->get('user');
 
-        foreach ($query->result_array() as $row)
-        {
-            echo $row['nickname'];
-            echo $row['phone'];
-            echo $row['birthday'];
-        }
 
-		// $data['data']   = $query;
+		$data['data']   = $query->result_array();
 
-		// $this->output
-		// 	->set_content_type('application/json')
-		// 	->set_output(json_encode($data, JSON_UNESCAPED_UNICODE))
-		// 	->_display();
-		// exit();
+		$this->output
+			->set_content_type('application/json')
+			->set_output(json_encode($data, JSON_UNESCAPED_UNICODE))
+			->_display();
+		exit();
     }
 }
