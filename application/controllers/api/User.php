f2703f0f96222705aaf5de53b0f932a0d23a8629
penglin <plin@qq.com>
2019-02-26 11:37:55 +0800

    1

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -9,8 +9,10 @@ class User extends CI_Controller
     }
     
     public function list(){
-        
-        $query = $this->db->query('SELECT user.id,user.nickname,user.phone,user.sex,user.birthday,user.type_id,user_type.label AS type_name,user.class_id,class.name AS class_name FROM user LEFT JOIN user_type ON user.type_id = user_type.id LEFT JOIN class ON user.class_id = class.id');
+
+		// $query = $this->db->query('SELECT user.id,user.nickname,user.phone,user.sex,user.birthday,user.type_id,user_type.label AS type_name,user.class_id,class.name AS class_name FROM user LEFT JOIN user_type ON user.type_id = user_type.id LEFT JOIN class ON user.class_id = class.id LIMIT 0,2');
+		
+		$query = $this->db->query('SELECT * FROM user COUNT');
 
 		$data['data']   = $query->result_array();
 
