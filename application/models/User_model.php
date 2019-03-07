f649f3720c170dd323b760e7fabcee70a9086165
penglin <plin@qq.com>
2019-03-07 15:00:38 +0800

    1

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -13,7 +13,6 @@ class User_model extends CI_Model {
 				$get = $this->input->get();
 
 				$query = $this->db->query('SELECT user.id,user.nickname,user.phone,user.sex,user.birthday,user.type_id,user_type.label AS type_name FROM user LEFT JOIN user_type ON user.type_id = user_type.id HAVING user.id > 0 LIMIT 0,10');
-				
 
         $data['data'] = $query->result_array();
         $data['get'] = $get;
