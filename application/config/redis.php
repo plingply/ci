24e544aef6259340bb3da4bafe30a678379cf65d
penglin <plin@qq.com>
2020-05-12 16:59:14 +0800

    2222

---
+++ application/config/redis.php
@@ -0,0 +1,13 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * Date: 2017/2/9
+ * Time: 13:32
+ */
+$config['socket_type'] = 'tcp';
+$config['host'] = '127.0.0.1';
+$config['password'] = NULL;
+$config['port'] = 6379;
+$config['timeout'] = 0;
